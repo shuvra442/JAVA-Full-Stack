a2ed5b532f1c4c699bf34e5931c7436160305dab
shuvra442 <shuvrapatra5@gmail.com>
2024-01-07 15:59:15 +0530

    throws in exception

---
+++ BasicOfJava/Exception/TrowsIoEx.java
@@ -0,0 +1,22 @@
+package BasicOfJava.Exception;
+
+ import java.io.IOException;
+
+class M{
+  void method() throws IOException{
+    throw new IOException("Here define the exception");
+  }
+}
+
+public class TrowsIoEx {
+  public static void main(String[] args)throws IOException{
+    try {
+      M obj = new M();
+      obj.method();
+      System.out.println("Normal flow");
+      
+    } catch (Exception e) {
+      System.out.println("Exception occure and catech here ");
+    }
+  }
+}
