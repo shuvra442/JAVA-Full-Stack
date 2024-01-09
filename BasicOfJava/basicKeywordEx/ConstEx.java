1e9d784734e7e045f472a83f163885a62e89419a
shuvra442 <shuvrapatra5@gmail.com>
2024-01-09 20:32:00 +0530

    constant as a final

---
+++ BasicOfJava/basicKeywordEx/ConstEx.java
@@ -0,0 +1,13 @@
+package BasicOfJava.basicKeywordEx;
+
+class ConstEx{
+  public static void main(String[] args) {
+    // final keyword is used to declare constant variable in java
+    // a constant variable can not be changed after initialization
+    final int a=30;
+    // if we can define a verialbe as const then we can not changed the value 
+    // a=40;
+    System.out.println("Value of a : "+a);
+    System.out.println("Value of a : "+a);
+  }
+}
